b005ebfb9e551a52cb800eac47e782602635361f
Samuel Mohler <smohler@pdx.edu>
2020-12-09 19:49:05 -0800

    refactored XMAS, much faster

---
+++ test_XMAS.jl
@@ -0,0 +1,8 @@
+# a generic tst file to write quick tests in which then get promoted to \tsts
+# to copy the test cases from www.adventofcode.com/2020/day/x use copy, i, shift+insert
+include("XMAS.jl")
+import .XMAS
+using .XMAS, Test
+@testset "XMAS" begin
+    @test XMAS.hack("src\\testdata\\xmas-5-127.txt", 5)==(127, 62)
+end
